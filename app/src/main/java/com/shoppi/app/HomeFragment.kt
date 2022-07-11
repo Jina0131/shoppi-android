223ca82b0a5add8bec2a6d907c852d50198f2494
Jina <chjina0131@gmail.com>
2022-07-11 21:59:27 +0900

    flagment 이용하여 화면 전환

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class HomeFragment: Fragment() {
@@ -14,5 +15,16 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+        button.setOnClickListener {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.add(R.id.container_main, ProductDetailFragment())
+            transaction.commit()
+        }
     }
 }
